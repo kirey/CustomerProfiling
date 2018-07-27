5cc0d5bcb9d7baf0eb87dcbdc25a2736a8e5552e
Natalija <kapljicaa@gmail.com>
2018-07-27 14:24:45 +0200

    B: added getProjectDetails

--- src/main/java/com/kirey/customerprofiling/api/restcontrollers/ProjectsController.java
+++ src/main/java/com/kirey/customerprofiling/api/restcontrollers/ProjectsController.java
@@ -3,6 +3,7 @@ package com.kirey.customerprofiling.api.restcontrollers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +33,13 @@ public class ProjectsController {
 		
 	}
 	
+	@RequestMapping(value = "/projectDetails/{id}", method = RequestMethod.GET)
+	public ResponseEntity<RestResponseDto> getProjectDetails(@PathVariable Integer id) {	
+		
+		return new ResponseEntity<RestResponseDto>(new RestResponseDto(projectDao.findById(id), HttpStatus.OK.value()), HttpStatus.OK);
+		
+	}
 	
 	
+	//TODO edit, create,  copy,  delete
 }
