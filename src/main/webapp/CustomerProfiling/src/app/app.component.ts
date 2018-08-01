da91445e024ac907bff4afd09f5789b2b7a816cd
cvetkovicm <m.cvetkovic@dyntechdoo.com>
2018-08-01 11:07:39 +0200

    F: Import Roboto font

--- src/main/webapp/CustomerProfiling/src/app/app.component.ts
+++ src/main/webapp/CustomerProfiling/src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { AuthService } from './shared/services/auth.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
 
