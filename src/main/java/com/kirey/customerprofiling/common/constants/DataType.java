665c02ca73759fa5f1770f7308e8404e5556a3be
Natalija <kapljicaa@gmail.com>
2018-08-02 11:00:34 +0200

    B: Changed DataType enum

--- src/main/java/com/kirey/customerprofiling/common/constants/DataType.java
+++ src/main/java/com/kirey/customerprofiling/common/constants/DataType.java
@@ -2,5 +2,14 @@ package com.kirey.customerprofiling.common.constants;
 
 public enum DataType {
 
-	NUMERIC,TEXT
+	NUMERIC("NUMERIC"),
+	TEXT("TEXT");
+	
+	private String desc;
+
+	public String getDesc()
+	{ return desc; }
+	
+	private DataType(String desc)
+	{ this.desc = desc; }
 }
