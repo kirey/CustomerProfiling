6f2f85ffe4ec82ea16469347d0206d7b20b8dde7
Natalija <kapljicaa@gmail.com>
2018-08-02 15:02:39 +0200

    B: Add new Algorithm with parameters

--- src/main/java/com/kirey/customerprofiling/api/restcontrollers/AlgorithmsController.java
+++ src/main/java/com/kirey/customerprofiling/api/restcontrollers/AlgorithmsController.java
@@ -1,16 +1,22 @@
 package com.kirey.customerprofiling.api.restcontrollers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kirey.customerprofiling.api.dto.RestResponseDto;
 import com.kirey.customerprofiling.data.dao.AlgorithmsDao;
+import com.kirey.customerprofiling.data.dao.ParametersDao;
+import com.kirey.customerprofiling.data.entity.Algorithms;
+import com.kirey.customerprofiling.data.entity.Parameters;
 
 @RestController
 @RequestMapping("/rest/algorithms")
@@ -18,6 +24,9 @@ public class AlgorithmsController {
 
 	@Autowired 
 	private AlgorithmsDao algorithmsDao;
+	
+	@Autowired 
+	private ParametersDao parametersDao;
 
 	@RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<RestResponseDto> getAllAlgorithms(){
@@ -29,5 +38,19 @@ public class AlgorithmsController {
 	public ResponseEntity<RestResponseDto> getAlgorithmDetails(@PathVariable Integer id){
 			
 		return new ResponseEntity<RestResponseDto>(new RestResponseDto(algorithmsDao.findById(id), HttpStatus.OK.value()), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<RestResponseDto> addAlgorithm(@RequestBody Algorithms algorithm){
+		
+		List<Parameters> parametersList = algorithm.getParameters();
+		algorithmsDao.attachDirty(algorithm);	
+
+		for (Parameters parameter : parametersList) {
+			parameter.setAlgorithm(algorithm);
+			parametersDao.attachDirty(parameter);
+		}
+				
+		return new ResponseEntity<RestResponseDto>(new RestResponseDto("Algorithm added", HttpStatus.OK.value()), HttpStatus.OK);
 	}
 }
