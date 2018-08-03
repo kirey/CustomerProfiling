53d0e188821531d27286d87ee0b12903038e9504
Milos Paunovic <m.paunovic@dyntechdoo.com>
2018-08-03 14:44:25 +0200

    B: Method for saving project with algorithms changed

--- src/main/java/com/kirey/customerprofiling/data/dao/ProjectAlgorithmsDao.java
+++ src/main/java/com/kirey/customerprofiling/data/dao/ProjectAlgorithmsDao.java
@@ -7,6 +7,8 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.kirey.customerprofiling.data.entity.Algorithms;
+import com.kirey.customerprofiling.data.entity.Projects;
 import com.kirey.customerprofiling.data.entity.ProjectsAlgorithms;
 
 @Repository(value = "projectAlgorithmsDao")
@@ -20,11 +22,27 @@ public class ProjectAlgorithmsDao extends KjcBaseDao {
 		entityClass = ProjectsAlgorithms.class;
 	}
 
-	
+	/**
+	 * Method for getting List of {@link ProjectsAlgorithms} by {@link Projects}
+	 * @param projectId - of {@link Projects}
+	 * @return {@link List}<{@link ProjectsAlgorithms}>
+	 */
 	public List<ProjectsAlgorithms> findByProject(Integer projectId) {
 		String hql = "from ProjectsAlgorithms pa where pa.project.id = :projectId";
 		List<ProjectsAlgorithms> listProjectAlgorithm = sessionFactory.getCurrentSession().createQuery(hql).setParameter("projectId", projectId).list();
 		return listProjectAlgorithm;
+	}
+
+	/**
+	 * Method for getting {@link ProjectsAlgorithms} by {@link Projects} and {@link Algorithms}
+	 * @param project
+	 * @param algorithm
+	 * @return {@link ProjectsAlgorithms}
+	 */
+	public ProjectsAlgorithms findByProjectAndAlgorithms(Projects project, Algorithms algorithm) {
+		String hql = "from ProjectsAlgorithms pa where pa.project.id = :projectId and pa.algorithm.id = :algorithmId";
+		ProjectsAlgorithms projectsAlgorithms = (ProjectsAlgorithms) sessionFactory.getCurrentSession().createQuery(hql).setParameter("projectId", project.getId()).setParameter("algorithmId", algorithm.getId()).uniqueResult();
+		return projectsAlgorithms;
 	}
 
 }
