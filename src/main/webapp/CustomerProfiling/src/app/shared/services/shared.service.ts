c70d89da936a2cfaa0d75a89b746fd4e6a19cd48
SERVICES-KIREY\anticm <marinamantic94@gmail.com>
2018-08-07 13:53:48 +0200

    F: Shared service - Initial commit; Shared service - completed ---> getting and setting datasetId and projectId;

---
+++ src/main/webapp/CustomerProfiling/src/app/shared/services/shared.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class SharedService {
+
+    /////////////////////////////////////////////////////////////////////////////////////// 
+    ////    This service has purpose to SET and GET       ////
+    ////     variables globally through application              ////
+    /////////////////////////////////////////////////////////////////////////////////////
+
+    // ToDo - Make global universal FUNCTION
+
+    projectId: number;
+    datasetId: number;
+
+    constructor() { }
+
+    // PROJECT ID
+    setProjectId(projectId: number) {
+        this.projectId = projectId;
+    }
+
+    getProjectId(projectId: boolean) {
+        return this.projectId;
+    }
+
+    // DATASET ID
+    setDatasetId(id: number) {
+        this.datasetId = id;
+    }
+
+    getDatasetId(id: boolean) {
+        return this.datasetId;
+    }
+}
