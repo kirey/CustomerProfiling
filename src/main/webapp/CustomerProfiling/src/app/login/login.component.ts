5c406fdd109a0aaaef25bd369a424162604e0fd6
cvetkovicm <m.cvetkovic@dyntechdoo.com>
2018-08-31 09:21:39 +0200

    F: SignUp form

--- src/main/webapp/CustomerProfiling/src/app/login/login.component.ts
+++ src/main/webapp/CustomerProfiling/src/app/login/login.component.ts
@@ -4,7 +4,6 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { SnackBarService } from '../shared/services/snackbar.service';
 
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,24 +11,49 @@ import { SnackBarService } from '../shared/services/snackbar.service';
   encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent implements OnInit {
-
   private loginForm: FormGroup;
   private username;
   private password;
+  public signupForm: FormGroup;
+  loginCard = true;
+  signup = false;
 
-  constructor(private _formBuilder: FormBuilder, private _router: Router, private _auth: AuthService, private _snackBarService: SnackBarService) {
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _router: Router,
+    private _auth: AuthService,
+    private _snackBarService: SnackBarService
+  ) {
     if (this._auth.isLoggedIn()) {
       this._router.navigate(['/dashboard']);
     }
+  }
+  openSignup() {
+    this.signup = true;
+    this.loginCard = false;
   }
 
   ngOnInit() {
     this.loginForm = this._formBuilder.group({
       username: ['', Validators.required],
-      password: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+    this.signupForm = this._formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', Validators.required],
+      password: ['', Validators.required]
     });
   }
-
+  signupSubmit() {
+    console.log(this.signupForm.value);
+    this.resetForm();
+  }
+  resetForm() {
+    this.signupForm.reset();
+  }
   login() {
     const loginObj = {
       username: this.username,
@@ -42,10 +66,10 @@ export class LoginComponent implements OnInit {
       },
       err => {
         if (localStorage.getItem('username')) {
-          localStorage.removeItem('username')
+          localStorage.removeItem('username');
         }
         this._snackBarService.openSnackBar(err['message'], 'Error');
       }
-    )
+    );
   }
 }
