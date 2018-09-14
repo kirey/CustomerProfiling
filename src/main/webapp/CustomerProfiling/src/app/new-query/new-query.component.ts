9844ffa841dea2cf7cf91f18ef7838a268828a9f
cvetkovicm <m.cvetkovic@dyntechdoo.com>
2018-09-14 12:59:57 +0200

    F: New Query, Query Result content implemented

--- src/main/webapp/CustomerProfiling/src/app/new-query/new-query.component.ts
+++ src/main/webapp/CustomerProfiling/src/app/new-query/new-query.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-new-query',
   templateUrl: './new-query.component.html',
-  styleUrls: ['./new-query.component.scss']
+  styleUrls: ['./new-query.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class NewQueryComponent implements OnInit {
 
